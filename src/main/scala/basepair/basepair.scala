d8c812f79ff0339e58e86496b0430889f9ead813
Jan Ferko <julyloov@gmail.com>
2012-11-17 17:06:21 +0100

    Assignement 3 finished

---
+++ src/main/scala/basepair/basepair.scala
@@ -0,0 +1,22 @@
+package basepair
+
+import commons._
+
+class DnaPairing {
+  def findPair(dnaString: String): String = dnaString.reverseMap {
+    x => x match {
+      case 'A' => 'T'
+      case 'T' => 'A'
+      case 'G' => 'C'
+      case 'C' => 'G'
+    }
+  }
+}
+
+object App {
+  def main(args: Array[String]): Unit = {
+    val dnaPairing = new DnaPairing
+    val runner = new Runner
+    runner.run("src/main/scala/basepair/rosalind_pair.txt", dnaPairing.findPair)
+  }
+}
